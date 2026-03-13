dddfa92f38e081536a38af2c880439ebac67b274
bui14 <143203446+bui14@users.noreply.github.com>
2026-03-13 17:12:38 +0800

    Update solution for unet-encoder-block

--- unet/unet-encoder-block/unet-encoder-block.py
+++ unet/unet-encoder-block/unet-encoder-block.py
@@ -1,13 +1,30 @@
 import numpy as np
+
+def kernel_init(Cin: int, out_channels: int):
+    n_in = 3 * 3 * Cin
+    std = np.sqrt(2/n_in)
+    return np.random.randn(3, 3, Cin, out_channels) * std
     
 def conv3x3(x: np.ndarray, out_channels: int):
     N, H, W, Cin = x.shape
+    he_normal = kernel_init(Cin, out_channels)
     output = np.zeros((N, H - 2, W - 2, out_channels))
-    return output
+    
+    for i in range(H - 2):
+        for j in range(W - 2):
+            window = x[:, i:i+3, j:j+3, :]
+            output[:, i, j, :] = np.tensordot(window, he_normal, axes=([1, 2, 3], [0, 1, 2]))
+    return np.maximum(output,0)
 
-def maxpooling(x: np.ndarray, out_channels: int):
+def maxpooling(x: np.ndarray):
     N, H, W, Cin = x.shape
-    output = np.zeros((N, H//2, W//2, out_channels))
+    output = np.zeros((N, H//2, W//2, Cin))
+
+    for i in range(H//2):
+        for j in range(W//2):
+            window = x[:, i*2 : i*2+2, j*2 : j*2+2, :]
+            output[:, i, j, :] = np.max(window, axis=(1, 2))
+            
     return output
     
 def unet_encoder_block(x: np.ndarray, out_channels: int) -> tuple:
@@ -19,6 +36,6 @@ def unet_encoder_block(x: np.ndarray, out_channels: int) -> tuple:
 
     skip_out = conv3x3(conv1_out, out_channels)
 
-    pool_out = maxpooling(skip_out, out_channels)
+    pool_out = maxpooling(skip_out)
 
     return pool_out, skip_out
